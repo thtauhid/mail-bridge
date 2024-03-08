5f65f3ea18732aced3605a95463b332a8197b6aa
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2024-03-08 14:55:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# TS-EXPRESS-STARTER
+
+## Description
+
+This is a starter project for a TypeScript Express server. It includes a basic setup for a server.
+
+## Features
+
+    - TypeScript
+    - Express
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import morgan from "morgan";
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});

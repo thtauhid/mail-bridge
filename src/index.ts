34f2fa203c4962763a6992e2fffd3ae240f62361
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2024-03-10 11:17:31 +0530

    Add retryCount to MailBridge constructor and implement checkConfig method

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,27 @@ export class MailBridge {
   private providers: PROVIDER[];
   private secrets: SECRETS;
   private defaultFrom: string;
+  /**
+   * The number of times to retry sending an email.
+   * Multiple retries are useful when the email provider is down.
+   * Multiple providers need to be configured for this to work.
+   */
+  private retryCount: number;
 
   constructor({
     providers,
     secrets,
     defaultFrom,
+    retryCount,
   }: {
     providers: PROVIDER[];
     secrets: SECRETS;
     defaultFrom: string;
+    retryCount?: number;
   }) {
     this.providers = providers;
     this.defaultFrom = defaultFrom;
+    this.retryCount = retryCount || 0;
 
     if (providers.includes("RESEND") && !secrets?.RESEND_API_KEY) {
       throw new Error(
@@ -41,5 +50,63 @@ export class MailBridge {
 
     // Use the provided provider
     return await sendEmail(email, provider, this.secrets);
+  }
+
+  /**
+   * Check the configuration of the MailBridge
+   */
+  async checkConfig() {
+    let report = {
+      providers: Array<PROVIDER>(),
+      errors: Array<string>(),
+      defaultFrom: undefined as string | undefined,
+      comment: "",
+    };
+
+    // Add all the providers to the report
+    for (let provider of this.providers) {
+      report.providers.push(provider);
+    }
+
+    // Check if the secrets are present for each provider
+
+    // Resend
+    if (this.providers.includes("RESEND") && !this.secrets.RESEND_API_KEY) {
+      report.errors.push("RESEND_API_KEY is required");
+    }
+
+    // Mailgun
+    if (this.providers.includes("MAILGUN") && !this.secrets.MAILGUN_API_KEY) {
+      report.errors.push("MAILGUN_API_KEY is required");
+    }
+
+    if (this.providers.includes("MAILGUN") && !this.secrets.MAILGUN_DOMAIN) {
+      report.errors.push("MAILGUN_DOMAIN is required");
+    }
+
+    // Default from
+    if (!this.defaultFrom) {
+      report.errors.push("Default from address is not configured");
+    } else {
+      // Check if the default from is a valid email address
+      if (!this.defaultFrom.includes("@")) {
+        report.errors.push("Default from address is not a valid email address");
+      }
+
+      report.defaultFrom = this.defaultFrom;
+    }
+
+    // Retry count
+    if (this.retryCount < 0) {
+      report.errors.push("Retry count cannot be negative");
+    }
+
+    if (report.errors.length === 0) {
+      report.comment = "MailBridge seems to be configured correctly";
+    } else {
+      report.comment = "MailBridge has some configuration errors";
+    }
+
+    return report;
   }
 }
